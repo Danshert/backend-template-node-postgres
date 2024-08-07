61794db9d7c2bccde8d33079ee2b589c60c37f55
Daniel Hernández <danielht97-@outlook.com>
2024-08-06 18:20:25 -0600

    JwtAdapter created.

---
+++ src/config/jwt.adapter.ts
@@ -0,0 +1,35 @@
+import jwt from 'jsonwebtoken';
+
+import { envs } from './envs';
+
+const JWT_SEED = envs.JWT_SEED;
+
+export class JwtAdapter {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	static generateToken(payload: any, duration: string = '2h') {
+		return new Promise((resolve) => {
+			jwt.sign(
+				payload,
+				JWT_SEED,
+				{
+					expiresIn: duration,
+				},
+				(error, token) => {
+					if (error) return resolve(null);
+
+					resolve(token);
+				},
+			);
+		});
+	}
+
+	static validateToken<T>(token: string): Promise<T | null> {
+		return new Promise((resolve) => {
+			jwt.verify(token, JWT_SEED, (error, decoded) => {
+				if (error) return resolve(null);
+
+				resolve(decoded as T);
+			});
+		});
+	}
+}
