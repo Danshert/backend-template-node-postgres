970207166e46f05a7f3a65a1fc60b57e397690c6
Daniel Hernández <danielht97-@outlook.com>
2024-09-11 16:46:45 -0600

    Create envs.test.ts

---
+++ tests/confg/envs.test.ts
@@ -0,0 +1,17 @@
+import { envs } from '../../src/config';
+
+describe('Tests in envs', () => {
+	test('should return env options', () => {
+		expect(envs).toEqual({
+			PORT: 3001,
+			POSTGRES_URL: expect.any(String),
+			POSTGRES_DB: 'TODO_DB_TEST',
+			JWT_SEED: expect.any(String),
+			SEND_EMAIL: expect.any(Boolean),
+			MAILER_SERVICE: 'gmail',
+			MAILER_EMAIL: expect.any(String),
+			MAILER_SECRET_KEY: expect.any(String),
+			WEB_SERVICE_URL: expect.any(String),
+		});
+	});
+});
