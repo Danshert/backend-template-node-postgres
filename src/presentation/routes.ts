93180e3390e6495366dfad007054069a37a344a1
Daniel Hernández <danielht97-@outlook.com>
2024-09-20 19:41:35 -0600

    Updated routes.

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -4,6 +4,8 @@ import { AuthRoutes } from './auth/routes';
 import { BoardRoutes } from './board/routes';
 import { LabelRoutes } from './label/routes';
 import { TaskRoutes } from './task/routes';
+import { FileUploadRoutes } from './file-uploads/routes';
+import { ImageRoutes } from './images/routes';
 
 export class AppRoutes {
 	static get routes(): Router {
@@ -13,6 +15,8 @@ export class AppRoutes {
 		router.use('/api/boards', BoardRoutes.routes);
 		router.use('/api/labels', LabelRoutes.routes);
 		router.use('/api/tasks', TaskRoutes.routes);
+		router.use('/api/upload', FileUploadRoutes.routes);
+		router.use('/api/images', ImageRoutes.routes);
 
 		return router;
 	}
