3bace7e78c212700792c23d652b3d10bd5e686a7
Daniel Hernández <danielht97-@outlook.com>
2024-09-20 19:40:30 -0600

    File uploads routes and controller created.

---
+++ src/presentation/file-uploads/controller.ts
@@ -0,0 +1,50 @@
+import { Request, Response } from 'express';
+
+import { CustomError } from '../../domain/errors';
+
+import { FileUploadService } from '../services';
+import { UploadedFile } from 'express-fileupload';
+
+export class FileUploadController {
+	// eslint-disable-next-line no-unused-vars
+	constructor(private readonly fileUploadService: FileUploadService) {}
+
+	private handleError = (error: unknown, response: Response) => {
+		if (error instanceof CustomError) {
+			return response
+				.status(error.statusCode)
+				.json({ error: error.message });
+		}
+
+		console.log(`${error}`);
+		return response.status(500).json({ error: 'Internal server error' });
+	};
+
+	uploadFile = (request: Request, response: Response) => {
+		const type = request.params.type;
+		const validTypes = ['users', 'tasks'];
+
+		if (!validTypes.includes(type)) {
+			return response.status(400).json({
+				error: `Invalid type: ${type}, valid ones ${validTypes}`,
+			});
+		}
+
+		const file = request.body.files[0] as UploadedFile;
+
+		this.fileUploadService
+			.uploadSingle(file, `uploads/${type}`)
+			.then((uploaded) => response.json(uploaded))
+			.catch((error) => this.handleError(error, response));
+	};
+
+	uploadMultipleFile = (request: Request, response: Response) => {
+		const type = request.params.type;
+		const files = request.body.files as UploadedFile[];
+
+		this.fileUploadService
+			.uploadMultiple(files, `uploads/${type}`)
+			.then((uploaded) => response.json(uploaded))
+			.catch((error) => this.handleError(error, response));
+	};
+}
---
+++ src/presentation/file-uploads/routes.ts
@@ -0,0 +1,28 @@
+import { Router } from 'express';
+
+import { AuthMiddleware } from '../middlewares/auth.middleware';
+
+import { FileUploadController } from './controller';
+import { FileUploadService } from '../services';
+import { FileUploadMiddleware, TypeMiddleware } from '../middlewares';
+
+export class FileUploadRoutes {
+	static get routes(): Router {
+		const router = Router();
+
+		const fileUploadService = new FileUploadService();
+		const controller = new FileUploadController(fileUploadService);
+
+		router.use([
+			AuthMiddleware.validateJWT,
+			FileUploadMiddleware.containFiles,
+			TypeMiddleware.validTypes(['users', 'tasks']),
+		]);
+
+		router.post('/single/:type', controller.uploadFile);
+
+		router.post('/multiple/:type', controller.uploadMultipleFile);
+
+		return router;
+	}
+}
