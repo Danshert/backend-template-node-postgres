cdbbc84116f98407b14f10cde14ff65d2f836471
Daniel Hernández <danielht97-@outlook.com>
2024-10-05 18:46:00 -0600

    Notification api docs added.

--- src/presentation/notification/routes.ts
+++ src/presentation/notification/routes.ts
@@ -22,8 +22,63 @@ export class NotificationRoutes {
 		const controller = new NotificationController(notificationService);
 
 		router.use([AuthMiddleware.validateJWT]);
-
+		/**
+		 * @swagger
+		 * /api/notifications:
+		 *   get:
+		 *     summary: Get notifications
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Notification
+		 *     parameters:
+		 *       - in: query
+		 *         name: boardId
+		 *         schema:
+		 *           type: object
+		 *           properties:
+		 *             boardId:
+		 *               type: string
+		 *           required:
+		 *             - boardId
+		 *     responses:
+		 *       201:
+		 *         description: Get notifications
+		 *       400:
+		 *         description: Invalid data
+		 *       500:
+		 *         description: Internal server error
+		 */
 		router.get('/', controller.getNotifications);
+
+		/**
+		 * @swagger
+		 * /api/notifications/{id}:
+		 *   put:
+		 *     summary: Mark notification as seen
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Notification
+		 *     parameters:
+		 *       - in: path
+		 *         name: id
+		 *         required: true
+		 *         description: Notification ID
+		 *         schema:
+		 *           type: string
+		 *     responses:
+		 *       200:
+		 *         description: Updated notification
+		 *       400:
+		 *         description: Invalid data
+		 *       401:
+		 *         description: Unauthorized
+		 *       404:
+		 *         description: Not found
+		 *       500:
+		 *         description: Internal server error
+		 */
 		router.put('/:id', controller.markNotificationAsSeen);
 
 		router.post('/subscription', controller.subscription);
