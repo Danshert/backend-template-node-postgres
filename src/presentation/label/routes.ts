0acbeb9eee904e622d58119a5d7d198e8f408b65
Daniel Hernández <danielht97-@outlook.com>
2024-10-05 18:44:49 -0600

    Label api docs added.

--- src/presentation/label/routes.ts
+++ src/presentation/label/routes.ts
@@ -12,23 +12,142 @@ export class LabelRoutes {
 		const labelService = new LabelService();
 		const controller = new LabelController(labelService);
 
-		router.get('/', [AuthMiddleware.validateJWT], controller.getLabels);
+		router.use([AuthMiddleware.validateJWT]);
 
-		router.get('/:id', [AuthMiddleware.validateJWT], controller.getLabels);
+		/**
+		 * @swagger
+		 * /api/labels/{boardId}:
+		 *   get:
+		 *     summary: Get label by id
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Label
+		 *     parameters:
+		 *       - in: path
+		 *         name: boardId
+		 *         required: true
+		 *         description: Label ID
+		 *         schema:
+		 *           type: string
+		 *     responses:
+		 *       200:
+		 *         description: Get label
+		 *       400:
+		 *         description: Invalid data
+		 *       401:
+		 *         description: Unauthorized
+		 *       404:
+		 *         description: Not found
+		 *       500:
+		 *         description: Internal server error
+		 */
+		router.get('/:id', controller.getLabels);
 
-		router.post('/', [AuthMiddleware.validateJWT], controller.createLabel);
+		/**
+		 * @swagger
+		 * /api/labels:
+		 *   post:
+		 *     summary: Create label
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Label
+		 *     requestBody:
+		 *       description: Label data
+		 *       required: true
+		 *       content:
+		 *         application/json:
+		 *           schema:
+		 *             type: object
+		 *             properties:
+		 *               boardId:
+		 *                 type: string
+		 *               name:
+		 *                 type: string
+		 *               color:
+		 *                 type: string
+		 *             required:
+		 *               - name
+		 *     responses:
+		 *       201:
+		 *         description: Label created
+		 *       400:
+		 *         description: Invalid data
+		 *       500:
+		 *         description: Internal server error
+		 */
+		router.post('/', controller.createLabel);
 
-		router.put(
-			'/:id',
-			[AuthMiddleware.validateJWT],
-			controller.updateLabel,
-		);
+		/**
+		 * @swagger
+		 * /api/labels/{id}:
+		 *   put:
+		 *     summary: Update label
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Label
+		 *     parameters:
+		 *       - in: path
+		 *         name: id
+		 *         required: true
+		 *         description: Label ID
+		 *         schema:
+		 *           type: string
+		 *     requestBody:
+		 *       description: Label data
+		 *       required: true
+		 *       content:
+		 *         application/json:
+		 *           schema:
+		 *             type: object
+		 *             properties:
+		 *               name:
+		 *                 type: string
+		 *               color:
+		 *                 type: string
+		 *             example:
+		 *               name: Label
+		 *     responses:
+		 *       201:
+		 *         description: Label updated
+		 *       400:
+		 *         description: Invalid data
+		 *       500:
+		 *         description: Internal server error
+		 */
+		router.put('/:id', controller.updateLabel);
 
-		router.delete(
-			'/:id',
-			[AuthMiddleware.validateJWT],
-			controller.deleteLabel,
-		);
+		/**
+		 * @swagger
+		 * /api/labels/{id}:
+		 *   delete:
+		 *     summary: Delete label
+		 *     security:
+		 *       - bearerAuth: []
+		 *     tags:
+		 *       - Label
+		 *     parameters:
+		 *       - in: path
+		 *         name: id
+		 *         required: true
+		 *         description: Label ID
+		 *         schema:
+		 *           type: string
+		 *     responses:
+		 *       200:
+		 *         description: Deleted label
+		 *       400:
+		 *         description: Invalid data
+		 *       401:
+		 *         description: Unauthorized
+		 *       404:
+		 *         description: Not found
+		 *       500:
+		 *         description: Internal server error
+		 */
+		router.delete('/:id', controller.deleteLabel);
 
 		return router;
 	}
